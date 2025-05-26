4e1c7d8da6f490da94673390e2f7f1e1337a2a02
ssabirovo <muhiddinsobirov957@gmail.com>
2025-05-26 09:12:03 +0500

    fix: fixed build error

--- src/pages/landing/components/index.ts
+++ src/pages/landing/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Navbar } from './Navbar';
-export { default as Hero } from './hero';
+export { default as Hero } from './Hero';
 export { default as About } from './About';
 export { default as Features } from './features';
